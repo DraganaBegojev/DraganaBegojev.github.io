1de610e58bc11c20ab3cb446d2bc79f6e051d0cc
DraganaBegojev <163642152+DraganaBegojev@users.noreply.github.com>
2025-10-03 10:42:17 +0200

    Adjust project description layout
    React app project description

--- js/app.js
+++ js/app.js
@@ -8,13 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
       img: "images/project-16.jpg",
       alt: "Portfolio Site Project",
       shortDescription: "A React-based image gallery app that fetches photos from the Pixabay API, featuring dynamic search, topic routes, and a modular component structure built with React Router and Vite.",
-      fullDescription: `This project is a React single-page application that functions as an interactive image gallery. Using React, Vite, and React Router, the app displays images fetched from the Pixabay API and allows users to navigate between predefined topic pages or search for custom keywords.
-      Technical highlights:
-        - Modular React Components – Container components manage state and pass data to reusable stateless components.
-        - Client-Side Routing – React Router enables seamless navigation between topic and search pages.
-        - API Integration – Fetch API retrieves image data dynamically from Pixabay, supporting search functionality.
-        - Custom Styling – Extended CSS to personalize the gallery layout and enhance usability.
-        - Single-Page Application (SPA) – Built with best practices for fast, responsive, and maintainable front-end architecture.`,
+      fullDescription: `This project is a React single-page application that functions as an interactive image gallery. Using React, Vite, and React Router, the app displays images fetched from the Pixabay API and allows users to navigate between predefined topic pages or search for custom keywords.<br>Technical highlights:
+      <br>- Modular React Components – Container components manage state and pass data to reusable stateless components.
+      <br> - Client-Side Routing – React Router enables seamless navigation between topic and search pages.
+      <br> - API Integration – Fetch API retrieves image data dynamically from Pixabay, supporting search functionality.
+      <br>- Custom Styling – Extended CSS to personalize the gallery layout and enhance usability.
+      <br>- Single-Page Application (SPA) – Built with best practices for fast, responsive, and maintainable front-end architecture.`,
       link: "https://react-gallery-app-flame.vercel.app/",
       github: "https://github.com/DraganaBegojev/React-Gallery-App"
     },
@@ -185,7 +184,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.description-btn').forEach(button => {
       button.addEventListener('click', () => {
         modalTitle.textContent = button.getAttribute('data-title');
-        modalBody.textContent = button.getAttribute('data-description');
+        modalBody.innerHTML = button.getAttribute('data-description');
       });
     });
   }
